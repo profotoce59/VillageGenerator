d0b55c43e5abec983e4de4285b9d83500564895d
profotoce59 <71013671+profotoce59@users.noreply.github.com>
2022-02-14 14:03:05 +0100

    Delete IMerger.java
    useless

--- src/main/java/reecriture/IMerger.java
+++
@@ -1,8 +0,0 @@
-package reecriture;
-
-public interface IMerger {
-    boolean forMergedIndexes(IDoubleListMerger.IConsumer p_197855_1_);
-    public interface IConsumer {
-        boolean merge(int p_merge_1_, int p_merge_2_, int p_merge_3_);
-    }
-}
